906a5953b55d485af5aa11c9a5c17442add32e7d
Gang Qiu <gangqiu0@gmail.com>
2020-03-25 13:42:55 -0400

    correctly sets initial grid to match porosity

--- percolation_monte_carlo/percolation_generator/grid.py
+++ percolation_monte_carlo/percolation_generator/grid.py
@@ -1,39 +1,76 @@
+import random
+
 class Grid():
     """
     The class that encapsulates the grid on which we'll be performing the percolation simulation
 
-    grid = Grid(
+    Grid is initialized with porosity = 0 (meaning it's all `1` or walls)
+    We will randomly add pores to match `initial_porosity` if given
     """
-    def __init__(self, width=None, height=None, porosity=0):
+    def __init__(self, width=None, height=None, initial_porosity=0):
         if width is None or height is None:
             raise ValueError(f"Must have numeric width and height. Got {width}, {height}")
 
         self._width = width
         self._height = height
-        self._target_porosity = porosity
+
+        # 2D array of 0/1 corresponding to the grid of pores/walls
+        # This is for visualization purposes only
         self._grid_matrix = self.__set_grid_matrix(width, height)
+
+        # 1D list of 0/1 corresponding to the grid of pores/walls
+        # For calculations it is more efficient to deal with a vector than a matrix
         self._grid_vector = self.__set_grid_vector(self._grid_matrix)
 
-    @property
-    def porosity(self):
+        # Sets that contain the vector indices for pores and walls
+        self._pores_vector_indices_set = set([])
+        self._walls_vector_indices_set = set([i for i in range(len(self._grid_vector))])
+
+        # If porosity is given, add a new pore to increase porosity
+        self._initialize_grid_to_match_porosity(initial_porosity)
+
+    # loop through entire matrix to get the porosity (slow, used for testing purposes)
+    def get_porosity_by_matrix(self):
         total_pores = 0
 
         for row in self._grid_matrix:
             for cell in row:
-                if cell == 1:
+                if cell == 0:
                     total_pores += 1
 
         return total_pores/self.area
+
+    # get porosity by calculating len of pore/wall vectors (fast)
+    @property
+    def porosity(self):
+        total_pores = len(self._pores_vector_indices_set)
+        return total_pores / self.area
 
     @property
     def area(self):
         return self._width * self._height
 
-    def __set_target_porosity(self, porosity):
-        pass
+    def _initialize_grid_to_match_porosity(self, porosity):
+        while self.porosity < porosity:
+            self.add_random_pore()
+            print(self.porosity)
+            print(self._grid_matrix)
+
+    def add_random_pore(self):
+        '''
+        adds a random pore to the grid. updates sets, vector, and grid
+        :return: vector index of the pore added
+        '''
+        new_pore_index = random.sample(self._walls_vector_indices_set, 1)[0]
+        self._walls_vector_indices_set.remove(new_pore_index)
+        new_pore_row, new_por_col = self.__vector_to_grid_coords(new_pore_index)
+
+        self._pores_vector_indices_set.add(new_pore_index)
+        self._grid_vector[new_pore_index] = 0
+        self._grid_matrix[new_pore_row][new_por_col] = 0
 
     def __set_grid_matrix(self, width, height):
-        matrix = [[0 for col in range(width)] for row in range(height)]
+        matrix = [[1 for col in range(width)] for row in range(height)]
 
         return matrix
 
--- tests/grid_test.py
+++ tests/grid_test.py
@@ -5,17 +5,18 @@ class GridTest(unittest.TestCase):
     def test_matrix(self):
         grid = Grid(width=4, height=2)
         expected_grid = [
-            [0,0,0,0],
-            [0,0,0,0],
+            [1,1,1,1],
+            [1,1,1,1],
         ]
 
         self.assertListEqual(grid._grid_matrix, expected_grid)
 
     def test_vector(self):
         grid = Grid(width=4, height=2)
-        expected_vector = [0,0,0,0,0,0,0,0]
+        expected_vector = [1,1,1,1,1,1,1,1]
         self.assertListEqual(grid._grid_vector, expected_vector)
 
     def test_grid_porosity(self):
-        grid = Grid(width=4, height=4, porosity=0.5)
+        grid = Grid(width=4, height=4, initial_porosity=0.5)
         self.assertAlmostEquals(grid.porosity, 0.5)
+        self.assertAlmostEquals(grid.get_porosity_by_matrix(), 0.5)
